279b6e58b1d1e6d581ae4ab73d7268bb0b6318ff
arda1228 <92720325+arda1228@users.noreply.github.com>
2023-07-28 13:21:58 +0100

    return message now appears on the screen below the submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,99 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import './App.css'; // Import the CSS file
+
+// interface FormData {
+//   carReg: string;
+//   startingPoint: string;
+//   destination: string;
+// }
+
+// const App: React.FC = () => {
+//   const [formData, setFormData] = useState<FormData>({
+//     carReg: '',
+//     startingPoint: '',
+//     destination: '',
+//   });
+
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await axios.post(process.env.REACT_APP_API_URL!, formData);
+//       console.log(response.data); // Optional: Display the response data
+
+//       // Reset the form
+//       setFormData({
+//         carReg: '',
+//         startingPoint: '',
+//         destination: '',
+//       });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       [name]: value,
+//     }));
+//   };
+
+//   return (
+//     <div className="container">
+//       <h1 className="title">summer</h1>
+//       <form onSubmit={handleSubmit} className="form">
+//         <div className="form-group">
+//           <label htmlFor="carReg" className="label">
+//             Car Registration:
+//           </label>
+//           <input
+//             type="text"
+//             id="carReg"
+//             name="carReg"
+//             value={formData.carReg}
+//             onChange={handleChange}
+//             className="input"
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="startingPoint" className="label">
+//             Starting Point:
+//           </label>
+//           <input
+//             type="text"
+//             id="startingPoint"
+//             name="startingPoint"
+//             value={formData.startingPoint}
+//             onChange={handleChange}
+//             className="input"
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="destination" className="label">
+//             Destination:
+//           </label>
+//           <input
+//             type="text"
+//             id="destination"
+//             name="destination"
+//             value={formData.destination}
+//             onChange={handleChange}
+//             className="input"
+//           />
+//         </div>
+//         <button type="submit" className="submit-btn">
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css'; // Import the CSS file
@@ -15,12 +111,14 @@ const App: React.FC = () => {
     destination: '',
   });
 
+  const [responseMessage, setResponseMessage] = useState<string>('');
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       const response = await axios.post(process.env.REACT_APP_API_URL!, formData);
-      console.log(response.data); // Optional: Display the response data
+      setResponseMessage(response.data); // Save the response data to the state
 
       // Reset the form
       setFormData({
@@ -88,6 +186,8 @@ const App: React.FC = () => {
           Submit
         </button>
       </form>
+
+      {responseMessage && <p className="response">{responseMessage}</p>}
     </div>
   );
 };
