2dd31a74cdd5505646b5efb2e115b8197e45fff6
arda1228 <92720325+arda1228@users.noreply.github.com>
2023-07-29 20:30:16 +0100

    added a dad joke

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,3 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import './App.css'; // Import the CSS file
-
-// interface FormData {
-//   carReg: string;
-//   startingPoint: string;
-//   destination: string;
-// }
-
-// const App: React.FC = () => {
-//   const [formData, setFormData] = useState<FormData>({
-//     carReg: '',
-//     startingPoint: '',
-//     destination: '',
-//   });
-
-//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-
-//     try {
-//       const response = await axios.post(process.env.REACT_APP_API_URL!, formData);
-//       console.log(response.data); // Optional: Display the response data
-
-//       // Reset the form
-//       setFormData({
-//         carReg: '',
-//         startingPoint: '',
-//         destination: '',
-//       });
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     setFormData((prevData) => ({
-//       ...prevData,
-//       [name]: value,
-//     }));
-//   };
-
-//   return (
-//     <div className="container">
-//       <h1 className="title">summer</h1>
-//       <form onSubmit={handleSubmit} className="form">
-//         <div className="form-group">
-//           <label htmlFor="carReg" className="label">
-//             Car Registration:
-//           </label>
-//           <input
-//             type="text"
-//             id="carReg"
-//             name="carReg"
-//             value={formData.carReg}
-//             onChange={handleChange}
-//             className="input"
-//           />
-//         </div>
-//         <div className="form-group">
-//           <label htmlFor="startingPoint" className="label">
-//             Starting Point:
-//           </label>
-//           <input
-//             type="text"
-//             id="startingPoint"
-//             name="startingPoint"
-//             value={formData.startingPoint}
-//             onChange={handleChange}
-//             className="input"
-//           />
-//         </div>
-//         <div className="form-group">
-//           <label htmlFor="destination" className="label">
-//             Destination:
-//           </label>
-//           <input
-//             type="text"
-//             id="destination"
-//             name="destination"
-//             value={formData.destination}
-//             onChange={handleChange}
-//             className="input"
-//           />
-//         </div>
-//         <button type="submit" className="submit-btn">
-//           Submit
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css'; // Import the CSS file
@@ -112,6 +16,7 @@ const App: React.FC = () => {
   });
 
   const [responseMessage, setResponseMessage] = useState<string>('');
+  const [dadJoke, setDadJoke] = useState<string>('');
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -126,6 +31,20 @@ const App: React.FC = () => {
         startingPoint: '',
         destination: '',
       });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleFetchDadJoke = async () => {
+    try {
+      const response = await axios.get('https://icanhazdadjoke.com/', {
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      setDadJoke(response.data.joke);
     } catch (error) {
       console.error(error);
     }
@@ -188,6 +107,9 @@ const App: React.FC = () => {
       </form>
 
       {responseMessage && <p className="response">{responseMessage}</p>}
+
+      <button onClick={handleFetchDadJoke}>Get Dad Joke</button>
+      {dadJoke && <p className="dad-joke">{dadJoke}</p>}
     </div>
   );
 };
